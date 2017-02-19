dab22f7552e4523f147e4b5bdf3126d58c78a471
David Broman <dbro@kth.se>
2017-02-19 20:35:41 +0100

    Update of grep test.

---
+++ exp/grep/readcount.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 32768
+
+char buf[SIZE];
+
+char c = ',';
+int count = 0;
+
+int main(){
+
+  FILE *f = fopen("f.txt","rb");
+  fseek(f,0,SEEK_END);
+  int s = ftell(f);
+  rewind(f);
+
+  for(int i=0; i<s; i+=SIZE){
+    int l = SIZE;
+    if(s-i <= SIZE){
+      l = s-i;
+      fread(buf,1,l,f);
+    }
+    else
+      fread(buf,1,SIZE,f);
+    
+    for(int j=0; j<l; j++){
+      if(buf[j] == c)
+        count++;
+    }
+  }
+  fclose(f);
+  printf("count = %d\n", count);
+}
