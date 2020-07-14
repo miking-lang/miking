6deb62a72eaf648b2c7a8187d9295cef113142c6
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2020-07-14 16:19:36 +0200

    Fix printing in Jupyter

--- src/boot/kernel/kernel.ml
+++ src/boot/kernel/kernel.ml
@@ -1,15 +1,47 @@
 open Repl
 open Jupyter_kernel
 
+let current_output = ref (BatIO.output_string ())
+
+let text_data_of_string str =
+  Client.Kernel.mime ~ty:"text/plain" str
+
 let init () =
-  Lwt.return (Py.initialize ())
+  let kernel_output_string str = BatIO.nwrite !current_output str in
+  let kernel_output_ustring ustr = ustr |> Ustring.to_utf8 |> kernel_output_string in
+  Mexpr.program_output := kernel_output_ustring;
+  Py.initialize ~version:3 ();
+  let py_init_print = "
+import sys
+from ocaml import ocaml_print
+class OCamlPrint:
+    def write(self, str):
+        ocaml_print(str)
+
+sys.stdout = OCamlPrint()
+" in
+  let m = Py.Import.add_module "ocaml" in
+  let py_ocaml_print args =
+    kernel_output_string (Py.String.to_string args.(0));
+    Py.none
+  in
+  Py.Module.set_function m "ocaml_print" py_ocaml_print;
+  ignore (Py.Run.eval ~start:Py.File py_init_print);
+  Lwt.return ()
 
 let exec ~count:_ code =
   try
     let ast = parse_prog_or_mexpr code in
     let result = ast |> repl_eval_ast |> Pprint.ustring_of_tm |> Ustring.to_utf8 in
-    Lwt.return (Ok { Client.Kernel.msg=Some(result)
-                   ; Client.Kernel.actions=[]})
+    let actions = [text_data_of_string result] in
+    let new_actions =
+      match BatIO.close_out !current_output with
+      | "" -> actions
+      | s -> text_data_of_string s :: actions
+    in
+    current_output := BatIO.output_string ();
+    Lwt.return (Ok { Client.Kernel.msg=None
+                   ; Client.Kernel.actions=new_actions})
   with e -> Lwt.return (Error (Printexc.to_string e))
 
 let main =
