595f8f8b1b99197fce9c176fbf8518d936e0bd6c
Anders Thuné <asse.97@gmail.com>
2020-07-15 16:37:33 +0200

    Fix plot behavior in kernel

--- src/boot/kernel/kernel.ml
+++ src/boot/kernel/kernel.ml
@@ -10,13 +10,16 @@ let text_data_of_string str =
 let kernel_output_string str = BatIO.nwrite !current_output str
 let kernel_output_ustring ustr = ustr |> Ustring.to_utf8 |> kernel_output_string
 
+let python_init = Py.initialize ~version:3 ()
+let ocaml_module = Py.Import.add_module "ocaml"
+
 (* Set Python's sys.stdout to our own ocaml function to handle Python prints *)
-let init_py_print m =
+let init_py_print () =
   let py_ocaml_print args =
     kernel_output_string (Py.String.to_string args.(0));
     Py.none
   in
-  Py.Module.set_function m "ocaml_print" py_ocaml_print;
+  Py.Module.set_function ocaml_module "ocaml_print" py_ocaml_print;
   ignore @@ Py.Run.eval ~start:Py.File "
 import sys
 from ocaml import ocaml_print
@@ -26,29 +29,29 @@ class OCamlPrint:
 
 sys.stdout = OCamlPrint()"
 
-let init_py_mpl m =
-  ignore (Py.Run.eval ~start:Py.File "
+let init_py_mpl () =
+  ignore @@ Py.Run.eval ~start:Py.File "
 import os
-os.environ['MPLBACKEND']='module://src.boot.kernel.mpl_backend'");
+os.environ['MPLBACKEND']='module://src.boot.kernel.mpl_backend'";
   let py_ocaml_show args =
     let data = Py.String.to_string args.(0) in
     other_actions := Client.Kernel.mime ~base64:true ~ty:"image/png" data :: !other_actions;
     Py.none
   in
-  Py.Module.set_function m "ocaml_show" py_ocaml_show
+  Py.Module.set_function ocaml_module "ocaml_show" py_ocaml_show
 
 let init () =
   Mexpr.program_output := kernel_output_ustring;
-  Py.initialize ~version:3 ();
-  let m = Py.Import.add_module "ocaml" in
-  init_py_print m;
-  init_py_mpl m;
+  Py.Module.set_function ocaml_module "after_exec" (fun _ -> Py.none);
+  init_py_print ();
+  init_py_mpl ();
   Lwt.return ()
 
 let exec ~count:_ code =
   try
     let ast = parse_prog_or_mexpr code in
     let result = ast |> repl_eval_ast |> Pprint.ustring_of_tm |> Ustring.to_utf8 in
+    ignore @@ Py.Module.get_function ocaml_module "after_exec" [||];
     let new_actions =
       match BatIO.close_out !current_output with
       | "" -> !other_actions
@@ -73,5 +76,5 @@ for creating embedded domain-specific and general-purpose languages"
       ~init:init
       ~exec:exec
       () in
-      let config = Client_main.mk_config ~usage:"Usage: hello" () in
+      let config = Client_main.mk_config ~usage:"Usage: kernel --connection-file {connection_file}" () in
       Lwt_main.run (Client_main.main ~config:config ~kernel:mcore_kernel)
--- src/boot/kernel/mpl_backend.py
+++ src/boot/kernel/mpl_backend.py
@@ -1,20 +1,48 @@
+import matplotlib
+import ocaml
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib._pylab_helpers import Gcf
 from ocaml import ocaml_show
 from io import BytesIO
 
-def show(close=None, block=None):
+matplotlib.interactive(True)
+
+def send_fig_to_kernel(fig, fmt='png'):
+    bytes_io = BytesIO()
+    fig.canvas.print_figure(bytes_io, format=fmt, bbox_inches='tight')
+    ocaml_show(bytes_io.getvalue())
+
+def show(close=True, block=None):
     """Show all figures as PNG payloads sent to the Jupyter frontend.
 
     Parameters
     ----------
-    close : Not used.
+    close : Close figures after sending
     block : Not used.
     """
     for figure_manager in Gcf.get_all_fig_managers():
         fig = figure_manager.canvas.figure
-        bytes_io = BytesIO()
-        fig.canvas.print_figure(bytes_io, format='png', bbox_inches='tight')
-        ocaml_show(bytes_io.getvalue())
+        send_fig_to_kernel(fig)
+    if close and Gcf.get_all_fig_managers():
+        matplotlib.pyplot.close('all')
+
+show._draw_called = False
+
+def draw_if_interactive():
+    if not matplotlib.is_interactive():
+        return
+    show._draw_called = True
+
+def flush_figures():
+    if show._draw_called:
+        show(True)
+        show._draw_called = False
 
 FigureCanvas = FigureCanvasAgg
+
+old_after_exec = ocaml.after_exec
+def new_after_exec():
+    old_after_exec()
+    flush_figures()
+
+ocaml.after_exec = lambda: new_after_exec()
