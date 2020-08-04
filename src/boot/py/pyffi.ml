c7767c2368431d737e2cbd8b935a8974796ae2e3
Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>
2020-08-04 15:55:34 +0200

    Add conversion of Python values, and add conversion for more types

--- src/boot/py/pyffi.ml
+++ src/boot/py/pyffi.ml
@@ -15,11 +15,12 @@ let mk_py fi p = TmConst(fi, CPy(p))
 
 let externals =
   let f p = mk_py NoInfo p in
-  [("pycall", f(Pycall(None,None))); ("pyimport", f(Pyimport))]
+  [("pycall", f(Pycall(None,None))); ("pyimport", f(Pyimport)); ("pyconvert", f(Pyconvert))]
 
 let arity = function
   | PyObject(_) -> 0
   | Pyimport -> 1
+  | Pyconvert -> 1
   | Pycall(None, None) -> 3 | Pycall(Some(_),None) -> 2 | Pycall(_,Some(_)) -> 1
 
 let rec val_to_python fi = function
@@ -32,7 +33,38 @@ let rec val_to_python fi = function
       try tmseq2ustring fi s |> Ustring.to_utf8 |> Py.String.of_string
       with _ -> Mseq.to_list s |> Py.List.of_list_map (val_to_python fi)
     end
+  | TmRecord(_,r) when r = Record.empty -> Py.none
+  | TmRecord(_,r) ->
+    Py.Dict.of_bindings_map
+      (fun x -> Py.String.of_string (Ustring.to_utf8 x))
+      (val_to_python fi)
+      (Record.bindings r)
   | _ -> raise_error fi "The supplied value cannot be used as a python argument"
+
+let rec python_to_val fi obj =
+  match Py.Type.get obj with
+  | Py.Type.Bool -> TmConst(fi,CBool(Py.Bool.to_bool obj))
+  | Py.Type.Int -> TmConst(fi,CInt(Py.Int.to_int obj))
+  | Py.Type.Long -> TmConst(fi,CInt(Py.Long.to_int obj))
+  | Py.Type.Float -> TmConst(fi,CFloat(Py.Float.to_float obj))
+  | Py.Type.None -> TmRecord(fi,Record.empty)
+  | Py.Type.Bytes | Py.Type.Unicode ->
+    let ustr = us (Py.String.to_string obj) in
+    TmSeq(fi,ustring2tmseq fi ustr)
+  | Py.Type.List ->
+    let lst = Py.List.to_list_map (python_to_val fi) obj in
+    TmSeq(fi,Mseq.of_list lst)
+  | Py.Type.Tuple ->
+    let lst = Py.Tuple.to_list_map (python_to_val fi) obj in
+    tuple2record fi lst
+  | Py.Type.Dict ->
+    let bindings = Py.Dict.to_bindings_string obj in
+    let mcore_bindings = List.map (fun (k,v) -> (us k, python_to_val fi v)) bindings in
+    let record = List.fold_right (fun (k,v) r -> Record.add k v r) mcore_bindings Record.empty in
+    TmRecord(fi, record)
+  | t ->
+    raise_error fi ("The supplied python value with type " ^ Py.Type.name t ^
+      " cannot be converted to an MCore value")
 
 let fail_constapp f v fi = raise_error fi
                           ("Incorrect application. function: "
@@ -53,6 +85,8 @@ let delta _ _ fi c v =
     | Pyimport, TmSeq(fi, lst) ->
         mk_py fi (PyObject(Py.import (tmseq2ustring fi lst |> Ustring.to_utf8)))
     | Pyimport,_ -> fail_constapp fi
+    | Pyconvert, TmConst(fi,CPy(PyObject(obj))) -> python_to_val fi obj
+    | Pyconvert,_ -> fail_constapp fi
     | Pycall(None, None),TmConst(_,CPy(PyObject(obj))) -> mk_py fi (Pycall(Some(obj), None))
     | Pycall(None, None),_ -> fail_constapp fi
     | Pycall(Some(m), None),TmSeq(fi,lst) ->
--- src/boot/py/pypprint.ml
+++ src/boot/py/pypprint.ml
@@ -5,6 +5,7 @@ open Printf
 let pprint = function
   | PyObject(v) -> us (Py.Object.to_string v)
   | Pyimport -> us "pyimport"
+  | Pyconvert -> us "pyconvert"
   | Pycall(None,None) -> us "pycall"
   | Pycall(Some(v),None) -> us (sprintf "pycall(%s)" (Py.Object.to_string v))
   | Pycall(Some(v),Some s) -> us (sprintf "pycall(%s, %s)" (Py.Object.to_string v) s)
