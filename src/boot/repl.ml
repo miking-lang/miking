13437a62ecf7cc752b8670bc9ab18494a1926008
Anders Thuné <46709166+andersthune@users.noreply.github.com>
2020-08-07 10:57:03 +0200

    Repl improvements and kernel preparations (#128)
    * Restructure files
    
    * Restructure repl.ml to allow calling functions from kernel
    
    * Remove exit on evaluating Cerror and add variable printing function
    
    * Fix subtle bug in repl.ml
    
    * Update printing of unit and None values
    
    * Refactor string conversion of errors
    
    * Allow setting the filename in repl parsing functions
    
    * Add autocompletion to REPL
    
    * Add persistent history to REPL
    
    * Fix directory permissions and handle unset home directory
    
    * Remove need to set home directory when line editing is off
    
    * Handle autocompletion of langs
    
    * Add keywords to autocompletion
    
    * Change REPL history file path to $HOME/.mcore_history
    
    Co-authored-by: Theo Puranen Åhfeldt <34159999+theoahfeldt@users.noreply.github.com>

--- src/boot/repl.ml
+++ src/boot/repl.ml
@@ -10,32 +10,56 @@
 
 open Ustring.Op
 open Printf
+open Eval
+open Mexpr
+open Ast
+open Mlang
+open Msg
+open Pprint
+
+module Option = BatOption
 
 let initial_prompt = ">> "
 let followup_prompt = " | "
 
 let no_line_edit = ref false
 
-module Option = BatOption
-
+let history_file =
+  if not !no_line_edit then
+    try Sys.getenv "HOME" ^ "/.mcore_history"
+    with Not_found -> failwith "$HOME is needed to save history, but it is unset. Either set it, or disable line editing with --no-line-edit"
+  else "@@@"
 
 (* Try to parse a string received by the REPL into an MCore AST *)
-let parse_mcore_string parse_fun str =
+let parse_mcore_string filename parse_fun str =
   let repl_tablength = 8 in
-  Lexer.init (us"REPL") repl_tablength;
+  Lexer.init (us filename) repl_tablength;
   let lexbuf = Lexing.from_string str in
   try Ok (parse_fun Lexer.main lexbuf)
   with Parsing.Parse_error -> Error (Lexing.lexeme lexbuf)
+
+let parse_prog_or_mexpr filename lines =
+  match parse_mcore_string filename Parser.main lines with
+  | Ok ast -> ast
+  | Error _ ->
+    match parse_mcore_string filename Parser.main_mexpr lines with
+    | Ok ast -> ast
+    | Error _ -> raise Parsing.Parse_error
 
 let read_input prompt =
   if !no_line_edit then
     (print_string prompt; read_line ())
   else
     match LNoise.linenoise prompt with
-      | None -> raise End_of_file
-      | Some str ->
-        LNoise.history_add str |> ignore;
-        String.trim str
+    | None -> raise End_of_file
+    | Some str ->
+      LNoise.history_add str |> ignore;
+      String.trim str
+
+let save_history_and_exit () =
+  if not !no_line_edit then
+    LNoise.history_save ~filename:history_file |> ignore;
+  exit 0
 
 let print_welcome_message () =
   print_endline "Welcome to the MCore REPL!";
@@ -50,9 +74,9 @@ let handle_command str =
    :q                          exit the REPL
    :h                          display this message|} in
   match str with
-    | ":q" -> exit 0
-    | ":h" -> print_endline help_message; true
-    | _ -> false
+  | ":q" -> save_history_and_exit ()
+  | ":h" -> print_endline help_message; true
+  | _ -> false
 
 (* Read and parse a toplevel or mexpr expression. Continues to read input
    until a valid expression is formed. Raises Parse_error if the expression
@@ -60,14 +84,14 @@ let handle_command str =
 let rec read_until_complete is_mexpr input =
   let new_acc () = sprintf "%s\n%s" input (read_input followup_prompt) in
   let parse_fun = if is_mexpr then Parser.main_mexpr else Parser.main in
-  match parse_mcore_string parse_fun input with
-    | Ok ast -> ast
-    | Error "" -> read_until_complete is_mexpr (new_acc ())
-    | Error _ ->
-      if is_mexpr then
-        raise Parsing.Parse_error
-      else
-        read_until_complete true input
+  match parse_mcore_string "REPL" parse_fun input with
+  | Ok ast -> ast
+  | Error "" -> read_until_complete is_mexpr (new_acc ())
+  | Error _ ->
+    if is_mexpr then
+      raise Parsing.Parse_error
+    else
+      read_until_complete true input
 
 (* Read and parse a multiline expression (:{\n ..lines.. \n:}).
    Returns None if the first line is not ":{" *)
@@ -81,12 +105,7 @@ let read_multiline first_line =
   if first_line = ":{" then
     let lines = List.fold_right (fun x a -> sprintf "%s\n%s" a x)
                                 (read_until_end []) "" in
-    match parse_mcore_string Parser.main lines with
-    | Ok ast -> Some ast
-    | Error _ ->
-      match parse_mcore_string Parser.main_mexpr lines with
-      | Ok ast -> Some ast
-      | Error _ -> raise Parsing.Parse_error
+    Some (parse_prog_or_mexpr "REPL" lines)
   else
     None
 
@@ -104,8 +123,127 @@ let rec read_user_input () =
    Returns updated environments along with evaluation result.
 *)
 let eval_with_envs (langs, nss, name2sym, sym2term) term =
-  let new_langs, flattened = Mlang.flatten_with_env langs term in
-  let new_nss, desugared = Mlang.desugar_post_flatten_with_nss nss flattened in
+  let new_langs, flattened = flatten_with_env langs term in
+  let new_nss, desugared = desugar_post_flatten_with_nss nss flattened in
   let new_name2sym, symbolized = Mexpr.symbolize_toplevel name2sym desugared in
   let new_sym2term, result = Mexpr.eval_toplevel sym2term symbolized in
   ((new_langs, new_nss, new_name2sym, new_sym2term), result)
+
+(* Wrap the final mexpr in a lambda application to prevent scope leak *)
+let wrap_mexpr (Program(inc, tops, tm)) =
+  let lambda_wrapper = TmLam(NoInfo, us"_", nosym, TyArrow(TyInt,TyDyn), tm) in
+  let new_tm = TmApp(NoInfo, lambda_wrapper, TmConst(NoInfo, CInt(0))) in
+  Program(inc, tops, new_tm)
+
+let repl_merge_includes = merge_includes (Sys.getcwd ()) []
+
+let repl_envs = ref (Record.empty, USMap.empty, builtin_name2sym, builtin_sym2term)
+
+let initialize_envs () =
+  let initial_envs, _ = Program([],[],TmConst(NoInfo,CInt(0)))
+                        |> add_prelude
+                        |> repl_merge_includes
+                        |> eval_with_envs !repl_envs in
+  repl_envs := initial_envs
+
+let repl_eval_ast prog =
+  let new_envs, result = prog
+    |> repl_merge_includes
+    |> wrap_mexpr
+    |> eval_with_envs !repl_envs in
+  repl_envs := new_envs;
+  result
+
+let repl_format tm =
+  match tm with
+  | TmRecord(_,x) when Record.is_empty x -> None
+  | TmConst(_,CPy(obj)) when Pyffi.is_none obj -> None
+  | _ -> Some(ustring_of_tm tm)
+
+(* Autocompletion *)
+let begins_at str pos =
+  let nonword_characters = Str.regexp "[][/\\\\(),={} \n\r\x0c\t]" in
+  try Str.search_backward nonword_characters str (pos-1) + 1
+  with Not_found -> 0
+
+let keywords = List.map fst Lexer.reserved_strings
+
+module USSet = Set.Make (Ustring)
+
+let keywords_and_identifiers () =
+  let extract_name id =
+    let sid =
+      match id with
+      | IdVar(s) -> s
+      | IdCon(s) -> s
+      | IdType(s) -> s
+      | IdLabel(s) -> s
+    in ustring_of_sid sid
+  in
+  let (_,nss,name2sym,_) = !repl_envs in
+  let names_without_langs = List.map (fun x -> x |> fst |> extract_name) name2sym in
+  let replace_name name mangled_name names =
+    names
+    |> USSet.add name
+    |> USSet.remove mangled_name
+  in
+  let process_lang lang ns names =
+    names
+    |> USSet.add lang
+    |> USMap.fold replace_name ns.constructors
+    |> USMap.fold replace_name ns.normals
+  in
+  names_without_langs
+  |> USSet.of_list
+  |> USMap.fold process_lang nss
+  |> USSet.to_seq
+  |> List.of_seq
+  |> List.map Ustring.to_utf8
+  |> (@) keywords
+
+let get_matches s = s
+  |> BatPervasives.flip BatString.starts_with
+  |> List.filter
+
+let get_completions str pos =
+  let start_pos = begins_at str pos in
+  let word_to_complete = BatString.slice ~first:start_pos ~last:pos str in
+  (start_pos, get_matches word_to_complete (keywords_and_identifiers ()))
+
+let completion_callback line_so_far ln_completions =
+  let start_pos,raw_completions = get_completions line_so_far (String.length line_so_far) in
+  let line_beginning = String.sub line_so_far 0 start_pos in
+  let completions = List.map (sprintf "%s%s" line_beginning) raw_completions in
+  List.iter (LNoise.add_completion ln_completions) completions
+
+let init_linenoise () =
+  if not !no_line_edit then (
+    LNoise.set_completion_callback completion_callback;
+    LNoise.history_load ~filename:history_file |> ignore)
+
+(* Run the MCore REPL *)
+let start_repl () =
+  let repl_print tm =
+    match repl_format tm with
+    | None -> flush stdout
+    | Some(str) -> uprint_endline str
+  in
+  let rec read_eval_print () =
+    try
+      read_user_input ()
+      |> repl_eval_ast
+      |> repl_print;
+      read_eval_print ()
+    with e ->
+      begin
+        match e with
+        | Sys.Break -> ()
+        | End_of_file -> save_history_and_exit ()
+        | _ -> uprint_endline @@ error_to_ustring e
+      end;
+      read_eval_print ()
+  in
+  print_welcome_message ();
+  init_linenoise ();
+  initialize_envs ();
+  read_eval_print ()
