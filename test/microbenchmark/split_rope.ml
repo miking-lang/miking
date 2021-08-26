a325a5974f7703bd623b2c5e9c6ed9c350c658df
Lars Hummelgren <larshum@kth.se>
2021-08-26 10:44:38 +0200

    Add benchmarks to show slicing improvements

---
+++ test/microbenchmark/split_rope.ml
@@ -0,0 +1,9 @@
+let rec sum acc s =
+  if Rope.length_array s == 0 then acc
+  else
+    let h = Rope.get_array s 0 in
+    let t = Rope.sub_array s 1 (Rope.length_array s) in
+    sum (acc + h) t
+
+let s = Rope.create 1000 (fun i -> i)
+let _ = Benchmarkcommon.repeat (fun () -> sum 0 s)
