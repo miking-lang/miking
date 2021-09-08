b2df1194a7c7d87ac611409a8945c5b41f3ae332
Anders Thuné <asse.97@gmail.com>
2021-09-08 21:53:05 +0900

    Remove `sundials` target from test-boot.mk

--- test-boot.mk
+++ test-boot.mk
@@ -9,23 +9,19 @@ base-files += $(wildcard stdlib/ad/*.mc)
 base-files += $(wildcard stdlib/parser/*.mc)
 base-files += $(wildcard stdlib/*.mc)
 
-sundials-files += $(wildcard test/sundials/*.mc)
-sundials-files += $(wildcard stdlib/sundials/*.mc)
-
 py-files = $(wildcard test/py/*.mc)
 
 ocaml-files = $(wildcard stdlib/ocaml/*.mc)
 
-.PHONY: base sundials py ocaml\
-  $(base-files) $(sundials-files) $(py-files) $(ocaml-files)
+.PHONY: base py ocaml\
+  $(base-files) $(py-files) $(ocaml-files)
 
 # Rules
 
 base: $(base-files)
-sundials: $(sundials-files)
 py: $(py-files)
 ocaml: $(ocaml-files)
 
 # File rule
-$(base-files) $(sundials-files) $(py-files) $(ocaml-files):
+$(base-files) $(py-files) $(ocaml-files):
 	-@MCORE_STDLIB=`pwd`/stdlib build/${BOOT_NAME} eval --test $@
