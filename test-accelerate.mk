3395e199d95d2bb94c121fbd0a1859ded2c43482
Lars Hummelgren <larshum@kth.se>
2021-12-01 20:47:33 +0100

    Run accelerate tests without acceleration as well

--- test-accelerate.mk
+++ test-accelerate.mk
@@ -4,5 +4,7 @@ include test-files.mk
 
 all: $(accelerate_files)
 
+# Run the accelerated files with and without enabling the accelerated mode
 $(accelerate_files):
 	@./make compile-test $@ "build/mi compile --accelerate --cpu-only"
+	@./make compile-test $@ "build/mi compile"
