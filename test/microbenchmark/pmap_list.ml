7625d1a0d0622d5bbaa9847d1e206642e0b7a817
Linnea Ingmar <lingmar@kth.se>
2022-01-25 14:05:23 +0100

    Run make fixc

--- test/microbenchmark/pmap_list.ml
+++ test/microbenchmark/pmap_list.ml
@@ -2,10 +2,9 @@
 
 let workload = 20
 
-let rec fibonacci n =
-  if n < 3 then 1
-  else (fibonacci (n - 1)) + (fibonacci (n - 2))
+let rec fibonacci n = if n < 3 then 1 else fibonacci (n - 1) + fibonacci (n - 2)
 
-let mapf n = List.rev (List.rev_map (fun _ -> fibonacci 20) (List.init n (fun i -> i)))
+let mapf n =
+  List.rev (List.rev_map (fun _ -> fibonacci 20) (List.init n (fun i -> i)))
 
 let _ = Benchmarkcommon.repeat (fun () -> mapf Map_n.n)
