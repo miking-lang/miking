716cf6737e33aeb63cc23030e73f9051769cbec2
William Rågstad <william.ragstad@gmail.com>
2022-05-19 16:17:52 +0200

    Assume curried foldl func

--- stdlib/javascript/intrinsics.js
+++ stdlib/javascript/intrinsics.js
@@ -21,7 +21,7 @@ const MExpr_JS_Intrinsics = Object.freeze({
   print: msg => console.log(MExpr_JS_Intrinsics.trimLastNewline(msg)),
   concat: lhs => rhs => lhs.concat(rhs),
   cons: elm => list => [elm].concat(list),
-  foldl: fun => init => list => list.reduce(fun, init),
+  foldl: fun => init => list => list.reduce((acc, e, i) => fun(acc)(e), init),
   char2int: c => c.charCodeAt(0),
   int2char: i => String.fromCharCode(i),
 
