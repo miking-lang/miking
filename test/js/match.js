53bff63c23979016ef15fb4a7f955ad7dfb92fe7
William Rågstad <william.ragstad@gmail.com>
2022-05-23 16:13:11 +0200

    Update test

--- test/js/match.js
+++ test/js/match.js
@@ -24,6 +24,9 @@ const MExpr_JS_Intrinsics = Object.freeze({
   foldl: fun => init => list => list.reduce((acc, e) => fun(acc)(e), init),
   char2int: c => c.charCodeAt(0),
   int2char: i => String.fromCharCode(i),
+  ref: value => { return { value: value } },
+  modref: ref => value => { ref.value = value; return ref; },
+  deref: ref => ref.value,
 
   // Helper Functions
   trimLastNewline: lst => lst[lst.length-1] === '\n' ? lst.slice(0, -1) : lst,
@@ -32,16 +35,16 @@ const MExpr_JS_Intrinsics = Object.freeze({
 
 let join = seqs => MExpr_JS_Intrinsics.foldl(MExpr_JS_Intrinsics.concat)("")(seqs);
 let printLn = s => {
-    MExpr_JS_Intrinsics.print(MExpr_JS_Intrinsics.concat(s)("\n"));
-  };
+  MExpr_JS_Intrinsics.print(MExpr_JS_Intrinsics.concat(s)("\n"));
+};
 let a = 1;
 let int2string = n => {
-    let int2string_rechelper = n1 => ((true === (n1 < 10)) ? [MExpr_JS_Intrinsics.int2char((n1 + MExpr_JS_Intrinsics.char2int('0')))] : (() => {
-          let d = [MExpr_JS_Intrinsics.int2char(((n1 % 10) + MExpr_JS_Intrinsics.char2int('0')))];
-          return MExpr_JS_Intrinsics.concat(int2string_rechelper((n1 / 10)))(d);
-        })());
-    return ((true === (n < 0)) ? MExpr_JS_Intrinsics.cons('-')(int2string_rechelper((-n))) : int2string_rechelper(n));
-  };
+  let int2string_rechelper = n1 => ((true === (n1 < 10)) ? [MExpr_JS_Intrinsics.int2char((n1 + MExpr_JS_Intrinsics.char2int('0')))] : (() => {
+      let d = [MExpr_JS_Intrinsics.int2char(((n1 % 10) + MExpr_JS_Intrinsics.char2int('0')))];
+      return MExpr_JS_Intrinsics.concat(int2string_rechelper((n1 / 10)))(d);
+    })());
+  return ((true === (n < 0)) ? MExpr_JS_Intrinsics.cons('-')(int2string_rechelper((-n))) : int2string_rechelper(n));
+};
 (({c: {b: b}} = {b: 2, c: {b: a}}) ? (() => {
     printLn(join([int2string(b), " == ", int2string(a)]));
     return printLn(((true === (a === b)) ? "true" : "false"));
