b63072552f92b2a70de60eab611e83bf864cdb6b
Anders Ågren Thuné <anders@aathn.org>
2023-07-03 19:55:49 +0200

    packaging: Add work on miking-pack-darwin

--- misc/packaging/default.nix
+++ misc/packaging/default.nix
@@ -1,7 +1,11 @@
 with import <nixpkgs> {};
+let ocamlBase = ocaml-ng.ocamlPackages_5_0.ocaml; in
 rec {
   miking = callPackage (import ./miking.nix) {};
   miking-shell = mkShell {
     buildInputs = [ miking ];
   };
+  ocaml-with-cc = ocamlBase.overrideAttrs (finalAttrs: previousAttrs: {
+    preConfigure = ''AS="cc -c" ASPP="cc -c"'';
+  });
 }
