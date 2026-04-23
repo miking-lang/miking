282bee6b149b0902cfbb1edd5880074a420b9691
Viktor Palmkvist <elegiosmail+github@gmail.com>
2026-04-23 15:56:27 +0200

    Update misc/packaging/flake.nix
    Co-authored-by: Oscar Eriksson <oscar.eriksson@protonmail.com>

--- misc/packaging/flake.nix
+++ misc/packaging/flake.nix
@@ -20,7 +20,7 @@
           devShells.default = pkgs.mkShell {
             name = "Miking dev shell";
             inputsFrom = [ packages.miking-lib packages.miking-unwrapped ];
-            buildInputs = [ pkgs.tup pkgs.ocamlformat_0_29_0 pkgs.ocamlPackages.owl pkgs.ocamlPackages.utop pkgs.gdb ];
+            buildInputs = with pkgs; [ tup ocamlformat_0_29_0 ocamlPackages.owl ocamlPackages.utop gdb ];
           };
         };
     in
